fbd3197bc7364d4280f0dabe6f0b67e7e986d946
Bas Randsdorp <104019748+BMRandsdorp@users.noreply.github.com>
2023-12-01 15:17:19 +0100

    Add files via upload

---
+++ CD/scripts/deploy_site.sh
@@ -0,0 +1,13 @@
+echo "Starting deployment of updated site"
+
+# navigate to directory of active site
+cd ../home
+# home is in #/ updating site contents fully
+# Pull contents from github
+git pull origin main
+# or git pull main.py
+
+# restart application
+systemctl restart my-application
+# Verify the application is running
+systemctl status my-application
