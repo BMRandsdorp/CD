4ab7e03d8518674ca8bf2388420bef41f88e3b57
Bas Randsdorp <b.m.randsdorp@hotmail.com>
2023-12-02 11:47:45 +0100

    changes to deploy script and yml file

--- scripts/deploy_site.sh
+++ scripts/deploy_site.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 echo "Starting deployment of updated site"
 
 # navigate to directory of active site
 cd ../home
 # home is in #/ updating site contents fully
 # Pull contents from github
-git pull origin main
+git pull origin master
 # or git pull main.py
 
 # restart application
