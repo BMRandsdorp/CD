66c7ae9125a3a181480ac922258f73f6f51c8e44
Bas Randsdorp <b.m.randsdorp@hotmail.com>
2023-12-02 12:27:47 +0100

    changed deploy permission

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,9 @@
 echo "Starting deployment of updated site"
-
 # navigate to directory of active site
 cd ..
 # home is in #/ updating site contents fully
 # Pull contents from github
 git pull origin master
-# or git pull main.py
-
 # restart application
 systemctl restart my-application
 # Verify the application is running
