42f4fc6caf3d1583bc18365a2bbee59c53566ead
Bas Randsdorp <b.m.randsdorp@hotmail.com>
2023-12-03 13:20:59 +0100

    test V3 added git status for check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,11 @@ echo "Starting deployment of updated site"
 # confirm working directory
 pwd 
 echo "moving to home directory and print contents" 
+# navigate to directory of active site
 cd home
 ls
-echo "moving to farm directory and print contents" 
-cd farm
-ls
-# navigate to directory of active site
-# cd ../home
-# home is in #/ updating site contents fully
+echo "testing git in home directory" 
+git status
 # Pull contents from github
 # git pull origin master
 # restart application
