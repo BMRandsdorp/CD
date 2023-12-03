47ca95c8d45707b408c036a0228f7ea638f00347
Bas Randsdorp <b.m.randsdorp@hotmail.com>
2023-12-03 14:46:52 +0100

    retesting ssh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,10 @@ echo "Starting deployment of updated site"
 
 # confirm working directory
 pwd 
+ls
 echo "moving to home directory and print contents" 
 # navigate to directory of active site
-cd home
+cd .. # home
 ls
 echo "moving to parent directory"
 cd ..
@@ -12,7 +13,7 @@ ls
 echo "run git status in directory" 
 git status
 # Pull contents from github
-git pull origin master
+# git pull origin master
 # restart application
 # systemctl restart nginx 
 # Verify the application is running
