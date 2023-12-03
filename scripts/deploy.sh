73abeb4cb4c8b1decc214ba46ca011f46cdaa8fb
Bas Randsdorp <b.m.randsdorp@hotmail.com>
2023-12-03 12:41:57 +0100

    adding workflow test and edited deploy for testing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,16 @@
 echo "Starting deployment of updated site"
+
+# confirm working directory
+pwd 
+
 # navigate to directory of active site
-cd ../home
+# cd ../home
 # home is in #/ updating site contents fully
 # Pull contents from github
-git pull origin master
+# git pull origin master
 # restart application
-systemctl restart nginx 
+# systemctl restart nginx 
 # Verify the application is running
-systemctl status farm
+# systemctl status farm
 
 exit
