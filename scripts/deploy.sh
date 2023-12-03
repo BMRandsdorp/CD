d7131290eb28eca8d5b4f056341d3ed67b44b53e
Bas Randsdorp <b.m.randsdorp@hotmail.com>
2023-12-03 15:05:05 +0100

    editing deploy for navigation and git status

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,17 @@ echo "Starting deployment of updated site"
 
 # confirm working directory
 pwd 
-ls
 echo "moving to home directory and print contents" 
 # navigate to directory of active site
-cd .. # home
+cd ..
 ls
-# echo "moving to parent directory"
-# cd ..
-# ls
+
 echo "run git status in directory" 
 git status
 # Pull contents from github
 # git pull origin master
 # restart application
-# systemctl restart nginx 
+# systemctl restart farm
 # Verify the application is running
 # systemctl status farm
 
